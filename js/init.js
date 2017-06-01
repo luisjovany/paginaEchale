8d79c494dede74a2e625558ebc16a2c505a91104
LuisJovany <jiovany@live.com.mx>
2017-05-31 20:45:44 -0500

    first commit

---
+++ js/init.js
@@ -0,0 +1,24 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+
+    var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
